f89d6d99455d4fdee3bdb133d80ccfee4e9b7fe2
Azhar Ali <azharbinanwar@gmail.com>
2024-11-22 21:52:10 +0500

    Upgrade bloc handler Add timer bloc

--- lib/core/bloc/helper.dart
+++ lib/core/bloc/helper.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
+
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:injectable/injectable.dart';
 
 class Helper<T> extends Cubit<T?> {
   Helper([super.initialState]);
@@ -6,4 +9,46 @@ class Helper<T> extends Cubit<T?> {
   void init(T? state) => emit(state);
 
   void update(T? state) => emit(state);
+}
+
+class Handler<T> extends Cubit<T> {
+  Handler(super.initialState);
+
+  void init(T state) => emit(state);
+
+  void update(T state) => emit(state);
+}
+
+@Injectable()
+class TimerHelper extends Cubit<int> {
+  TimerHelper([super.initialState = 0]);
+
+  Timer? _timer;
+
+  void start(int timeInSeconds, {Duration duration = const Duration(seconds: 1)}) {
+    emit(timeInSeconds);
+    _timer?.cancel();
+    _timer = Timer.periodic(
+      duration,
+          (Timer timer) {
+        if (timeInSeconds <= 0) {
+          cancel.call();
+          emit(0);
+        } else {
+          timeInSeconds = timeInSeconds - duration.inSeconds;
+          emit(timeInSeconds);
+        }
+      },
+    );
+  }
+
+  void cancel() {
+    _timer?.cancel();
+  }
+
+  @override
+  Future<void> close() {
+    _timer?.cancel();
+    return super.close();
+  }
 }
